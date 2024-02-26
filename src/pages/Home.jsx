9e34d7388e4f4d64fc1b9e0c67ed4df4a4be8692
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 00:54:25 +0300

    Translated the text in the Home component from Turkish to English.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,15 +7,13 @@ const Home = () => {
       <div className="bg-blue-500 text-white py-16">
         <div className="container mx-auto flex flex-col max-sm:space-y-10 sm:flex-row items-center justify-between">
           <div className="w-2/3 sm:w-1/2">
-            <h1 className="text-5xl font-bold mb-4">Todo Uygulaması</h1>
-            <p className="text-lg mb-4">
-              Görevlerinizi düzenleyin ve takip edin!
-            </p>
+            <h1 className="text-5xl font-bold mb-4">Todo App</h1>
+            <p className="text-lg mb-4">Organise and track your tasks!</p>
             <Link
               to="/sign-up"
               className="bg-white text-blue-500 py-2 px-4 rounded-full font-semibold transition duration-300 hover:bg-blue-500 hover:text-white"
             >
-              Hemen Başla
+              Get Started
             </Link>
           </div>
           <div className="w-2/3 sm:w-1/2">
